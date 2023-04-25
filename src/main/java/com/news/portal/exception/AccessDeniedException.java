ec37cc5d963927ddc09c7ea120fa5a160ba605d9
AidaTynybek <tynybekkyzyaida@gmail.com>
2023-04-25 10:32:33 +0600

    add AccessDeniedException.class

---
+++ src/main/java/com/news/portal/exception/AccessDeniedException.java
@@ -0,0 +1,8 @@
+package com.news.portal.exception;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
+
